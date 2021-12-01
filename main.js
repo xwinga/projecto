8a68300d401d32754a95eab6cec5949ac6b25507
xwinga <47614779+xwinga@users.noreply.github.com>
2021-12-01 16:48:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function addUser() {
+    player1_name= document.getElementById("input1").value;
+    player2_name= document.getElementById("input2").value;
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location="quiz.html";
+    }
+  
